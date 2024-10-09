dc8eb2e277ecd57cb77c92b81e7428b4ad59e207
20098910 <20098910@mai.wit.ie>
2024-10-09 10:30:38 +0100

    Week 5 Lab 1 - Added filtering to the driver list

--- app/src/main/java/com/example/f1geek/MainActivity.kt
+++ app/src/main/java/com/example/f1geek/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.f1geek
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.f1geek.model.Driver
@@ -27,34 +33,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DriverList(modifier: Modifier = Modifier) {
-
-    val roland = Driver(
-        firstName = "Roland", surname = "Ratzenberger", abbreviatedName = "RAT", number = 32
-    )
-    val jochen = Driver(
-        firstName = "Jochen", surname = "Rindt", abbreviatedName = "RIN", number = 22
-    )
-    val jules = Driver(
-        firstName = "Jules", surname = "Bianchi", abbreviatedName = "BIA", number = 17
+    val drivers = listOf(
+        Driver(
+            firstName = "Roland", surname = "Ratzenberger", abbreviatedName = "RAT", number = 32
+        ),
+        Driver(firstName = "Jochen", surname = "Rindt", abbreviatedName = "RIN", number = 22),
+        Driver(firstName = "Jules", surname = "Bianchi", abbreviatedName = "BIA", number = 17)
     )
 
-    var drivers = ArrayList<Driver>()
-    drivers.add(roland)
-    drivers.add(jochen)
-    drivers.add(jules)
+    var filterText by rememberSaveable  { mutableStateOf("") }
+    println("Hello, i'm recomposing")
 
     Column {
-        TextField(value = "", onValueChange = {}, label = { Text("Search") })
-        Text("${roland.firstName} ${roland.surname}")
-        Text("${jochen.firstName} ${jochen.surname}")
-        Text("${jules.firstName} ${jules.surname}")
-    }
-
-    Column {
-        for(driver in drivers)
-        {
+        TextField(
+            value = filterText,
+            onValueChange = { value -> filterText = value },
+            label = { Text("Search") }
+        )
+        drivers.filter { it.firstName.contains(filterText, true) }.forEach { driver ->
             Text("${driver.firstName} ${driver.surname}")
-
         }
     }
 }
