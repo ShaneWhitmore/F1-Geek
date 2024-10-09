1dd426528e55e86aaec68451d88df491f268b72f
20098910 <20098910@mai.wit.ie>
2024-10-09 11:11:43 +0100

    Week 5 Lab 1 - Created DriverStore

--- app/src/main/java/com/example/f1geek/MainActivity.kt
+++ app/src/main/java/com/example/f1geek/MainActivity.kt
@@ -3,47 +3,39 @@ package com.example.f1geek
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.f1geek.model.Driver
+import com.example.f1geek.model.seedDriverStore
 import com.example.f1geek.ui.theme.F1GeekTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             F1GeekTheme {
-                DriverList()
+                val driverStore = seedDriverStore()
+                DriverList(drivers = driverStore.drivers)
             }
         }
     }
 }
 
+
 @Composable
-fun DriverList(modifier: Modifier = Modifier) {
-    val drivers = listOf(
-        Driver(
-            firstName = "Roland", surname = "Ratzenberger", abbreviatedName = "RAT", number = 32
-        ),
-        Driver(firstName = "Jochen", surname = "Rindt", abbreviatedName = "RIN", number = 22),
-        Driver(firstName = "Jules", surname = "Bianchi", abbreviatedName = "BIA", number = 17)
-    )
-
-    var filterText by rememberSaveable  { mutableStateOf("") }
+fun DriverList(drivers: List<Driver>, modifier: Modifier = Modifier) {
+    var filterText by rememberSaveable { mutableStateOf("") }
     println("Hello, i'm recomposing")
-
     Column {
         TextField(
             value = filterText,
@@ -60,6 +52,8 @@ fun DriverList(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     F1GeekTheme {
-        DriverList()
+        val driverStore = seedDriverStore()
+        DriverList(drivers = driverStore.drivers)
     }
+
 }
---
+++ app/src/main/java/com/example/f1geek/model/DriverStore.kt
@@ -0,0 +1,26 @@
+package com.example.f1geek.model
+
+class DriverStore(val drivers: List<Driver> = mutableListOf())
+
+fun seedDriverStore(): DriverStore{
+    // TODO: add 10 drivers to a mutable list and return an initialised driver store
+    // You can make up your own formula 1 drivers or use real ones, it's up to you
+
+    val drivers = mutableListOf(
+        Driver(firstName = "Lewis", surname = "Hamilton", abbreviatedName = "HAM", number = 44),
+        Driver(firstName = "Max", surname = "Verstappen", abbreviatedName = "VER", number = 1),
+        Driver(firstName = "Charles", surname = "Leclerc", abbreviatedName = "LEC", number = 16),
+        Driver(firstName = "Sergio", surname = "Perez", abbreviatedName = "PER", number = 11),
+        Driver(firstName = "George", surname = "Russel", abbreviatedName = "RUS", number = 63),
+        Driver(firstName = "Carlos", surname = "Sainz", abbreviatedName = "SAI", number = 55),
+        Driver(firstName = "Lando", surname = "Norris", abbreviatedName = "NOR", number = 4),
+        Driver(firstName = "Fernando", surname = "Alonso", abbreviatedName = "ALO", number = 14),
+        Driver(firstName = "Oscar", surname = "Piastri", abbreviatedName = "PIA", number = 81),
+        Driver(firstName = "Esteban", surname = "Ocon", abbreviatedName = "OCO", number = 31)
+    )
+
+
+
+
+    return DriverStore(drivers)
+}
